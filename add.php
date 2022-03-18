937a6c61d2d1809481634b2db72d2bb33181de8b
ToniJukkola <n1juto00@students.oamk.fi>
2022-03-18 15:25:35 +0200

    descriptionin lisäys toimii

--- add.php
+++ add.php
@@ -14,6 +14,7 @@ $input = json_decode(file_get_contents('php://input'));
 $description = filter_var($input->description,FILTER_SANITIZE_SPECIAL_CHARS);
 
 
+
 try {
 $db = new PDO('mysql:host=localhost;dbname=shoppinglist;charset=utf8','root','');
 $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
@@ -21,6 +22,7 @@ $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 $query = $db->prepare('insert into item(description) values (:description)');
 $query->bindValue(':description',$description,PDO::PARAM_STR);
 $query->execute();
+
 
 header('HTTP/1.1 200 OK');
 $data = array('id'=> $db->lastInsertId(),'description' => $description);
